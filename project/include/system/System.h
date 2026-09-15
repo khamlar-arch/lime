13c7666021b4dedd378dca1ea5aecd4c0f074683
khamlar-arch <khamlar8527@gmail.com>
2026-09-15 07:43:48 +0700

    Update System.h

--- project/include/system/System.h
+++ project/include/system/System.h
@@ -40,7 +40,7 @@ namespace lime {
 			static bool GetIOSTablet ();
 			#endif
 			static int GetNumDisplays ();
-			static int GetDeviceOrientation ();
+			// static int GetDeviceOrientation ();
 			static std::wstring* GetPlatformLabel ();
 			static std::wstring* GetPlatformName ();
 			static std::wstring* GetPlatformVersion ();
@@ -54,7 +54,7 @@ namespace lime {
 			#if defined(HX_WINDOWS) && !defined (HX_WINRT)
 			static bool SetWindowsConsoleMode (int handleType, int mode);
 			#endif
-			static void EnableDeviceOrientationChange(bool enable);
+			// static void EnableDeviceOrientationChange(bool enable);
 
 		private:
 
